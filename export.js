ed03665fc88f26775633f1a7efa36d86f2e5a6a4
parmstrong62387 <projectpatgt@gmail.com>
2017-02-12 13:51:29 -0500

    Added prompts for download audio and download export file

--- export.js
+++ export.js
@@ -1,22 +1,77 @@
 var exportProgram = {
-	downloadAudio: true, //Set to true to download the associated audio files
-	downloadExportFile: false, //Set to true to download a file containing the export in ANKI format
-	index: 0, //Do not modify
-	$els: null, //Do not modify
-	startTime: null //Do not modify
+	'downloadAudio': true, //Set to true to download the associated audio files
+	'downloadExportFile': false, //Set to true to download a file containing the export in ANKI format
+	'index': 0, //Do not modify
+	'$els': null, //Do not modify
+	'startTime': null, //Do not modify
+	'initialized': false, //Do not modify
+	'prompts': [
+		{
+			'title': 'Download Audio?',
+			'key': 'downloadAudio'
+		},
+		{
+			'title': 'Download Export File?',
+			'key': 'downloadExportFile'
+		}
+	]
 };
 
 function runProgram() {
+	if (!exportProgram.initialized) {
+		$('head').append('<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css">');
+		for (var i = 0; i < exportProgram.prompts.length; i++) {
+			initDialog(i);
+		}
+		exportProgram.initialized = true;
+	}
+
 	console.clear();
 	exportProgram.index = 0;
 	exportProgram.$els = $('table.vocab-table:has(input[type=checkbox]:checked)');
-	exportProgram.startTime = new Date();
 
 	if (exportProgram.$els.length === 0) {
 		console.warn('No vocabulary selected.');
 	} else {
-		$(document).bind("ajaxSuccess", expandNextVocabularyWord);
-		expandNextVocabularyWord();
+		openDialog(0);
+	}
+}
+
+function beginExport() {
+	exportProgram.startTime = new Date();
+	$(document).bind("ajaxSuccess", expandNextVocabularyWord);
+	expandNextVocabularyWord();
+}
+
+function initDialog(index) {
+	var title = exportProgram.prompts[index].title;
+	var key = exportProgram.prompts[index].key;
+	var id = 'dialog-' + key;
+	$('body').append('<div data-index="' + index + '" id="' + id + '" title="' + title + '"></div>');
+	$('#' + id).dialog({
+		'autoOpen': false,
+		'buttons' : {
+			'Yes' : function() {
+				exportProgram[key] = true;
+				$(this).dialog("close");
+				var index = Number($(this).data('index'));
+				openDialog(index+1);
+			},
+			'No' : function() {
+				exportProgram[key] = false;
+				$(this).dialog("close");
+				var index = Number($(this).data('index'));
+				openDialog(index+1);
+			}
+		}
+	});
+}
+
+function openDialog(index) {
+	if (index < exportProgram.prompts.length) {
+		$('#dialog-' + exportProgram.prompts[index].key).dialog('open');
+	} else {
+		beginExport();
 	}
 }
 
@@ -26,9 +81,15 @@ function expandNextVocabularyWord() {
 
 	if (index < $els.length) {
 		console.info('Expanding ' + (index+1) + ' out of ' + $els.length + '...');
-		$els.eq(index).find('span.fa-expand').click();
-
+		var $el = $els.eq(index);
 		exportProgram.index++;
+
+		if ($el.find('span.fa-expand').length > 0) {
+			$el.find('span.fa-expand').click();
+		} else {
+			expandNextVocabularyWord();
+		}
+		
 	} else {
 		$(document).unbind("ajaxSuccess", expandNextVocabularyWord);
 		performExport();
